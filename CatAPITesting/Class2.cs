cbb74b1cdee517cc57bc0098e76fb97db9137497
lshanka <91688000+lshanka@users.noreply.github.com>
2021-10-02 20:44:00 +0100

    New Project Created
    New project created with two classes

---
+++ CatAPITesting/Class2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Cats
+{
+	public Cats()
+	{
+
+        public int MyProperty { get; set; }
+	    public int MyProperty { get; set; }
+}
+}
---
+++ CatAPITesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CatAPITesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
